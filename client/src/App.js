de53c3511d33141024bd7066c92fd6646b502409
ashychiu <ashychiu@gmail.com>
2022-01-12 19:10:17 -0800

    Initial setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
