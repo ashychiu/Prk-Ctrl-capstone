e0c81decf0c2e7dc153584e548553443b1cd3de0
ashychiu <ashychiu@gmail.com>
2022-01-15 21:29:03 -0800

    Server: Create JWT Authentication. Add dependencies - bcrypt, cookieParser, jsonwebtoken

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,12 @@ const express = require("express");
 const userRouter = express.Router();
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+// const jwt = require("jsonwebtoken");
+const { createTokens, validateToken } = require("../JWT");
+
+userRouter.use(cookieParser());
 
 const readFile = () => {
   const userData = fs.readFileSync("./data/users.json");
@@ -28,7 +34,7 @@ const getUser = (id) => {
 };
 
 //API Get single user
-userRouter.get("/:id", (req, res) => {
+userRouter.get("/profile/:id", (req, res) => {
   const { id } = req.params;
   const foundUser = getUser(id);
   if (!foundUser) {
@@ -38,10 +44,11 @@ userRouter.get("/:id", (req, res) => {
 });
 
 //Add new user
-userRouter.post("/add", (req, res) => {
-  const { firstName, lastName, email, unitNumber, status, phone } = req.body;
+userRouter.post("/signup", (req, res) => {
+  const { firstName, lastName, email, unitNumber, status, phone, password } =
+    req.body;
 
-  if (!email || !firstName || !lastName || !unitNumber) {
+  if (!email || !firstName || !lastName || !unitNumber || !password) {
     return res.status(400).send("Starred fields are required");
   }
   if (phone.length < 10) {
@@ -52,19 +59,48 @@ userRouter.post("/add", (req, res) => {
   if (!email.includes("@") || !email.includes(".")) {
     return res.status(400).send("Please input a valid email");
   }
-  const newUser = {
-    id: uuid(),
-    firstName,
-    lastName,
-    unitNumber,
-    phone,
-    email,
-    status,
-  };
+  bcrypt.hash(password, 10).then((hash) => {
+    const newUser = {
+      id: uuid(),
+      firstName,
+      lastName,
+      unitNumber,
+      phone,
+      email,
+      status,
+      password: hash,
+    };
+    userList.push(newUser);
+    writeFile(userList);
+    return res.status(201).json(userList);
+  });
+});
 
-  userList.push(newUser);
-  writeFile(userList);
-  return res.status(201).json(userList);
+//User login
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const foundUser = await userList.find((user) => user.email === email);
+  if (!foundUser) {
+    return res.status(404).send("User does not exist, please sign up first.");
+  }
+  const passwordOnFile = foundUser.password;
+  bcrypt.compare(password, passwordOnFile).then((match) => {
+    if (!match) {
+      res.status(400).send("Incorrect password!");
+    } else {
+      const accessToken = createTokens(foundUser); //Call the function created on JWT.js
+      res.cookie("accessToken", accessToken, {
+        maxAge: 2629800000, //one month in milliseconds
+        httpOnly: true,
+      });
+      res.send("Login successfully!");
+    }
+  });
+});
+
+//Get user profile
+userRouter.get("/profile", validateToken, (req, res) => {
+  res.json("profile");
 });
 
 //Update single user by id
